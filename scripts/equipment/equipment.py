6bedf30d41d4eb0f0f36ed0a1a730452f8cd2ecc
OpenSeesame <49232973+OpenSeesame@users.noreply.github.com>
2022-12-16 20:06:37 +0900

    Delete scripts/equipment directory

--- scripts/equipment/equipment.py
+++
@@ -1,8 +0,0 @@
-class Equipment:
-    def __init__(self, name='weapon_name'):
-        self.__name = name
-
-    def get_name(self):
-        return self.__name
-    def set_name(self, name):
-        self.__name = name
